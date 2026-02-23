c9b82103623f65d1850ed85b92ae4a6394eb31d3
Samuele Ferri <samuele1301@gmail.com>
2026-02-23 10:23:52 +0100

    refactor in SimpleBankAccountTest done

--- basic-refactoring-exercise/src/test/java/SimpleBankAccountTest.java
+++ basic-refactoring-exercise/src/test/java/SimpleBankAccountTest.java
@@ -3,6 +3,7 @@ import example.model.BankAccount;
 import example.model.SimpleBankAccount;
 
 import org.junit.jupiter.api.*;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 /**
@@ -12,11 +13,15 @@ class SimpleBankAccountTest {
 
     private AccountHolder accountHolder;
     private BankAccount bankAccount;
+    private double amount;
+    private int notExistingAccountHolderId;
 
     @BeforeEach
     void beforeEach(){
         accountHolder = new AccountHolder("Mario", "Rossi", 1);
         bankAccount = new SimpleBankAccount(accountHolder, 0);
+        amount = 100;
+        notExistingAccountHolderId = 2;
     }
 
     @Test
@@ -26,28 +31,29 @@ class SimpleBankAccountTest {
 
     @Test
     void testDeposit() {
-        bankAccount.deposit(accountHolder.id(), 100);
+        bankAccount.deposit(accountHolder.id(), amount);
         assertEquals(100, bankAccount.getBalance());
     }
 
     @Test
     void testWrongDeposit() {
-        bankAccount.deposit(accountHolder.id(), 100);
-        bankAccount.deposit(2, 50);
+        bankAccount.deposit(accountHolder.id(), amount);
+        bankAccount.deposit(notExistingAccountHolderId, amount);
         assertEquals(100, bankAccount.getBalance());
     }
 
     @Test
     void testWithdraw() {
-        bankAccount.deposit(accountHolder.id(), 100);
-        bankAccount.withdraw(accountHolder.id(), 70);
+        bankAccount.deposit(accountHolder.id(), amount);
+        amount = 70;
+        bankAccount.withdraw(accountHolder.id(), amount);
         assertEquals(30, bankAccount.getBalance());
     }
 
     @Test
     void testWrongWithdraw() {
-        bankAccount.deposit(accountHolder.id(), 100);
-        bankAccount.withdraw(2, 70);
+        bankAccount.deposit(accountHolder.id(), amount);
+        bankAccount.withdraw(notExistingAccountHolderId, amount);
         assertEquals(100, bankAccount.getBalance());
     }
 }
